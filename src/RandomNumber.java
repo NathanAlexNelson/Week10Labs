3ce781a37829a0bc522a8acd6ceba283a812bc58
SAC-CS112-Nelson-Nathan <Nathanalexnelson@gmail.com>
2015-10-28 20:29:45 -0700

    swf
    sdf

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -15,7 +15,16 @@ public class RandomNumber
 	public int GetANumber(int lo, int hi)
 	{
 		
-		int randomNumber = lo + (int) (Math.random() * hi);
+		int randomNumber = lo + (int) (Math.random() * (hi - lo));
 		return randomNumber;		
 	}
+	/*public int SetLowNumber(int low)
+	{
+		
+		
+	}
+	public int SetLowNumber(int high)
+	{
+		
+	}*/
 }
